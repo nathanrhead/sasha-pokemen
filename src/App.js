993d53253bd1369cb51202ced8a6154f8fb72f18
Nathan Rhead Cox <nrc4@hotmail.com>
2022-01-04 17:41:48 -0800

    Adding some code.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import GetPokemon from './components/get-pokemen';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <main>
+        <GetPokemon />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/display-pokemon.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class GetPokemen extends React.Component {
+  state = {
+    pokemon: {
+      name: '',
+      height: 0,
+      weight: 0,
+      order: 0,
+      abilities: [],
+      moves: [],
+      sprites: [],
+      stats: [],
+      types: [],
+    },
+  };
+
+  render() {
+    
+    return(
+      <div></div>
+    );
+  }
+}
